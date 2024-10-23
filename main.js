3d840b0282430ddfc53b6ecc70fe3719f598cb5a
martin tolaba <juantolaba70@gmail.com>
2024-10-23 19:46:59 -0300

    master

---
+++ main.js
@@ -0,0 +1,43 @@
+let nombre
+let numero
+let edad 
+let apellido
+
+const url = "www.google.com"
+
+
+nombre = "martin"
+numero= "1","3","4","6"
+edad = 33
+apellido = "tolaba"
+
+
+
+
+console.log(nombre)
+console.log(numero)
+console.log(edad)
+console.log(apellido)
+console.log(url)
+
+
+
+
+
+
+alert("bienvenido")
+
+confirm("¿queres facturar eso?")
+
+
+prompt("¿cual es tu nombre?")
+
+
+//array//
+const zapatillas = ["adidas","fila", "nike", "reebok"]
+const arraynumero1 = ["1","3","4","6"]
+
+
+console.log(zapatillas)
+console.log(arraynumero1)
+console.log(arrayproduct)
